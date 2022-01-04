90d97ef4d5ae9e83e69e25e6aed5d6aa68ac2b0c
devi854 <79244706+devi854@users.noreply.github.com>
2022-01-04 11:13:41 +0530

    Add files via upload

---
+++ mymodule/inputvariables.tf
@@ -0,0 +1,24 @@
+variable "subsccription_id" {
+    type = string
+    default = "730114f3-3c96-423d-b203-108734964640"
+    description = "azure subscription id"
+}
+variable "client_id" {
+    type = string
+    default = "17decd9c-cdd0-49cc-8e8d-9c78447c2152"
+    description = "azure client id"
+  
+}
+variable "client_secret" {
+    type = string
+    default = "qmy7Q~d.Y6IYhgXr0JgrHxQea5FAtHw6u21Y2"
+    description = "azure secret id"   
+
+  
+}
+variable "tenant_id" {
+    type = string
+    default = "ede58b21-37a8-4b40-b1ed-3b62f51309af"
+    description = "azure tenet id"
+  
+}
